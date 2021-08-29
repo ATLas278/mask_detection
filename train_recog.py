58f1a36b0202c3540e60341d49b9334c5062cef5
Atlas <realatlas2022@gmail.com>
2021-08-28 23:34:04 -0400

    base model done, plotting and saving draft model, working on opencv

--- train_recog.py
+++ train_recog.py
@@ -1,3 +1,4 @@
+from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator # Generate batches of tensor image data with real-time data augmentation.
 from tensorflow.keras.applications import MobileNetV2 # CNN architecture that works well w/mobile devices as well
 from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
@@ -5,63 +6,71 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam # stochastic gradient descent method that is based on adaptive estimation of first-order and second-order moments.
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input # Preprocesses a tensor or Numpy array encoding a batch of images.
 from tensorflow.keras.preprocessing.image import img_to_array, load_img 
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.utils import to_categorical # converts a class vector (integers) to a binary class matrix
-from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from imutils import paths
 
+from imutils import paths
+import cv2
 import numpy as np
 import pandas as pd
 import os
+import splitfolders
+import tensorflow as tf
 import matplotlib.pyplot as plt
+%matplotlib inline
 plt.style.use('ggplot')
 plt.rcParams.update({'font.size': 16})
 plt.rcParams['savefig.dpi'] = 200
 plt.rcParams['axes.facecolor'] = 'white'
 plt.rcParams['grid.color'] = 'lightgrey'
 
-# init the initial learning rate & number of epochs to train for
 init_lr = 1e-4
-epochs = 20
+epochs = 40
 bs = 32
+image_size = (192,192)
 
-DIRECTORY = r"data"
 CATEGORIES = ["with_mask","without_mask","mask_weared_incorrect"]
 
 print("[INFO] loading images...")
 
-# get the list of images in our dataset directory, 
-# then init the list of data and class images
-data = []
-labels = []
+# MobileNetV2 CNN architecture model
+def build_model(num_classes, img_size=192):
+    # load MobileNetV2 network, ensuring the head FC (Fully Connected) layer sets are left off
+    inputs = Input(shape=(192,192,3))    
+    baseModel = MobileNetV2(input_shape=(img_size,img_size,3),weights="imagenet",include_top=False, input_tensor=inputs)
+    
+    
+    # create the head of the model that will be placed on top of the base model
+    headModel = baseModel.output
+    headModel = AveragePooling2D(pool_size=(6,6))(headModel)
+    headModel = Flatten(name="flatten")(headModel)
+    headModel = Dense(128, activation="relu")(headModel)
+    headModel = Dropout(0.5)(headModel)
+    headModel = Dense(64, activation="relu")(headModel)
+    headModel = Dropout(0.5)(headModel)
+    headModel = Dense(num_classes, activation="softmax")(headModel)
+    
+    # place the head FC model on top of the base model (this will become the actual model we will train)
+    model = Model(inputs=baseModel.input, outputs=headModel)
 
-for category in CATEGORIES:
-    path = os.path.join(DIRECTORY, category)
-    # looping over image paths and prepocessing the images
-    for img in os.listdir(path):
-        img_path = os.path.join(path,img)
-        image = load_img(img_path,target_size=(175,175)) # resize all images, target size
-        image = img_to_array(image)
-        image = preprocess_input(image)
-        data.append(image)
-        labels.append(category)
+    # lopp over all layers in the base model and freeze them so they will not be updated during the first training process
+    for layer in baseModel.layers:
+        layer.trainable=False
         
-# one-hot encoding on the labels (1 and 0) binary format
-mlb = MultiLabelBinarizer()
-labels = mlb.fit_transform(labels)
+    # compile model
+    print("[INFO] compiling model...")
+    opt = Adam(lr=init_lr,decay=init_lr / epochs)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    
+    return model
 
-# convert data to arrays b/c hidden layers only accept this format
-data = np.array(data,dtype="float32")
-labels = np.array(labels)
-
-# split the data
-X_train, y_train, X_test, y_test = train_test_split(data, labels, test_size=0.2, stratify=labels, random_state=42)
-
-# construct the training image generator for data augmentation
-# recreating the images
-
-aug = ImageDataGenerator(
+# Pipeline for image augmentation
+datagen = ImageDataGenerator(
         rotation_range=20,
         zoom_range=0.15,
         width_shift_range=0.2,
@@ -69,6 +78,34 @@ aug = ImageDataGenerator(
         shear_range=0.15,
         horizontal_flip=True,
         fill_mode="nearest")
+    
+train = datagen.flow_from_directory('split_data/train', class_mode='categorical',target_size=(192,192), batch_size=bs)
+test = datagen.flow_from_directory('split_data/test',class_mode='categorical',target_size=(192,192), batch_size=bs)
+val = datagen.flow_from_directory('split_data/val',class_mode='categorical',target_size=(192,192), batch_size=bs)
 
-# load MobileNetV2 network, ensuring the head FC (Fully Connected) laery sets are left off
-baseModel = MobileNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(175,175,3)))
+x_train, y_train = train.next()
+x_test,y_test = test.next()
+x_val, y_val = val.next()
+
+num_classes = len(CATEGORIES)
+
+model = build_model(num_classes)
+
+# train head of the network
+print("[INFO] training head...")
+H = model.fit(
+        train,
+        steps_per_epoch=len(train) // bs,
+        validation_data=val,
+        batch_size=bs,
+        epochs=epochs,
+        callbacks=tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                  verbose=1,
+                                                  patience=4,
+                                                  restore_best_weights=True)
+)
+print("[INFO] evaluating network...")
+predIdxs = model.predict(test, batch_size=bs)
+
+# for each img in the testing set, we need to find idx of label w/corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
